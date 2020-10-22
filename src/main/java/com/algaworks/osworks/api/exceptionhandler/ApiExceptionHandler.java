04851e6ae8360d5872bb21f038edb8894e3d644b
tyupoid <thiago_tyupoid@hotmail.com>
2020-10-22 18:03:42 -0300

    Semana Algaworks

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.algaworks.osworks.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<Problema.Campos> campos = new ArrayList<Problema.Campos>();
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			String nome = ((FieldError) error).getField();
+			campos.add(new Problema.Campos(nome, mensagem));
+		}
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Um ou mais campo estao invalidos. faca o prenchimento correto  e tente novamente");
+		problema.setDataHora(LocalDateTime.now());
+		problema.setCampos(campos);
+
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+}
